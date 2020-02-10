0ae4ae6b15fbd51c19cbe56760c2fd7850b05fae
KafeinFaita <lynyrd.andres47@gmail.com>
2020-02-10 12:35:23 +0800

    added favicon and included loading status for each pokemon

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -4,20 +4,25 @@ const PokemonList = ({ url }) => {
 
     const [pokeImage, setPokeImage] = useState('');
     const [pokeName, setPokeName] = useState('');
-    const [pokeType, setPokeType] = useState([])
+    const [pokeType, setPokeType] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
+
         fetch(url).then(res => res.json()).then(data => {
             setPokeImage(data.sprites.front_default);
             setPokeName(data.name);
             setPokeType(data.types.map(type => type.type.name));
+            setIsLoading(false);
         })
     }, [url])
     
     return (       
         <div className="pokemon">
             <div className="img-container">
-                {pokeImage ? <img src={pokeImage} alt=""/> : <p style={{fontWeight: 'bold'}}>NO IMAGE AVAILABLE</p>}
+
+                {isLoading ? <p style={{fontWeight: 'bold'}}>LOADING</p> : pokeImage ? <img src={pokeImage} alt=""/> : <p style={{fontWeight: 'bold'}}>NO IMAGE AVAILABLE</p>}
             </div> 
             
             <div className="desc-container">
